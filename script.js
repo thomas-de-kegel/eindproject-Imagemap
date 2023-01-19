5348b2e8aaef9c092635aca5c521fa0cbf418638
thomas-de-kegel <thomas.dekegel@student.intecbrussel.be>
2023-01-19 13:26:05 +0100

    further testing with Jquery

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-function clickedMe(item) {
-    var mapName;
-    mapName = $(item).parent().attr('name');
-    $('img').each(function(){
-        if($(this).attr('usemap') == '#'+mapName){
-            alert($(this).attr('id'));            
-        }       
-    });
-}
+$(document).ready(function () {
+  console.log("Loaded and ready!");
+});

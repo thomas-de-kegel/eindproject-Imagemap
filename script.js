053cc8550824f29773cf36ed6ed6a7de870b9f82
thomas-de-kegel <thomas.dekegel@student.intecbrussel.be>
2023-01-19 14:31:24 +0100

    h1 now displays selected part

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ $(document).ready(function () {
 
   $("area").click(function(){
     console.log($(this).attr("title"));
+    $("#selectionDisplay").text($(this).attr("title"));
   })
 
-  $("h1").on({
-    mouseenter: function(){
-        $(this).css("color", "#1e90ff")
-    },
-    mouseleave: function(){
-        $(this).css("color","black")
-    }
-  })
+//   $("h1").on({
+//     mouseenter: function(){
+//         $(this).css("color", "#1e90ff")
+//     },
+//     mouseleave: function(){
+//         $(this).css("color","black")
+//     }
+//   })
 });

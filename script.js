bd2718cb5695b1e4693b81a29844093f1928e7cc
thomas-de-kegel <thomas.dekegel@student.intecbrussel.be>
2023-01-19 14:22:31 +0100

    further scripting + css

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
   console.log("Loaded and ready!");
+
+  $("area").click(function(){
+    console.log($(this).attr("title"));
+  })
+
+  $("h1").on({
+    mouseenter: function(){
+        $(this).css("color", "#1e90ff")
+    },
+    mouseleave: function(){
+        $(this).css("color","black")
+    }
+  })
 });

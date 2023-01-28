df4694f0af5cfc8a230bc34f2e137a3fe3027866
thomas-de-kegel <thomas.dekegel@student.intecbrussel.be>
2023-01-28 20:04:21 +0100

    Notes & Css updated

--- script.js
+++ script.js
@@ -60,6 +60,8 @@ $(document).ready(function () {
   $("#new-issue-form").submit(function (e) {
     e.preventDefault();
     console.log("Issue created");
+
+    //Convert time to readable format
     function formattedTime(date) {
       const hours = String(date.getHours()).padStart(2, "0");
       const minutes = String(date.getMinutes()).padStart(2, "0");
